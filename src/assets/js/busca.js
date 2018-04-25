6a1612a116847ac1048cd6b3013f4713e035ccc0
viniwalsh <vmw.walsh@gmail.com>
2018-04-24 23:49:40 -0300

    fix json

---
+++ src/assets/js/busca.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#busca_place').select2({
+        placeholder: "Buscar por..."
+    });
+
+});
---
+++ src/assets/js/translate.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'jv', includedLanguages: 'en,es', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element')
+}
+</script>
